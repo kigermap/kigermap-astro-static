607357670afc0fd4e2c7eb6c23c015333e141c54
greenhaha <greenhaha@users.noreply.github.com>
2026-06-13 07:09:08 +0000

    deploy: ff8be1b08f255cde912910f1dcba17ac9ebf42f9 source-repo: kigermap/kigermap-astro kigermap/kigermap-astro@ff8be1b08f255cde912910f1dcba17ac9ebf42f9

---
+++ _astro/App.DC6y6k6I.js
@@ -0,0 +1,1 @@
+import{ak as t}from"./App.Bkp8LKEN.js";import"./index.BAnu8Lhe.js";export{t as default};
---
+++ _astro/kanban1.BE67tj8p.js
@@ -0,0 +1,1 @@
+const t={src:"https://cdn-astro-static.kigermap.cn/_astro/kanban1.wjPYA3n9.webp",width:800,height:1182,format:"webp"};export{t as K};
